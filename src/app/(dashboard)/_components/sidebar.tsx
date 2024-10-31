86da07bfc8689107b3970975a96a6aeb7299e733
Rakin Bhuyan <rabhuyan@uab.edu>
2024-10-30 23:20:32 -0500

    removed unused libraries

--- src/app/(dashboard)/_components/sidebar.tsx
+++ src/app/(dashboard)/_components/sidebar.tsx
@@ -10,20 +10,19 @@ import {
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
-  SidebarGroupAction,
   SidebarGroupContent,
   SidebarGroupLabel,
   SidebarMenu,
   SidebarMenuButton,
-  SidebarMenuItem,
+  SidebarMenuItem
 } from "@/components/ui/sidebar";
 import { SignOutButton } from "@clerk/nextjs";
 import { useQuery } from "convex/react";
-import { PlusIcon, User2Icon } from "lucide-react";
+import { User2Icon } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { api } from "../../../../convex/_generated/api";
 import { NewDirectMessage } from "./new-direct-message";
-import { usePathname } from "next/navigation";
 
 
 export function DashboardSidebar() {
